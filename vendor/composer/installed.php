d664b6111730f968d68b54547271b41441774a56
Kandasamy Thivaan tkandasa <thivaan.kandasamy@stud.hslu.ch>
2023-11-09 07:50:22 +0100

    Init Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'fileeye/pel' => array(
+            'pretty_version' => '0.9.20',
+            'version' => '0.9.20.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fileeye/pel',
+            'aliases' => array(),
+            'reference' => '1675cbd44e89ff8f01b9576cf32ec1b4a4a67ede',
+            'dev_requirement' => false,
+        ),
+    ),
+);
